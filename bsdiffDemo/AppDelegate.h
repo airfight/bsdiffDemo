1a6f61c402aa99cc1e0f22791dcf39a2465eb3e4
zgy朱光阳 <gy.zhu@ctrip.com>
2019-03-22 14:18:34 +0800

    Initial Commit

---
+++ bsdiffDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  bsdiffDemo
+//
+//  Created by zgy on 2019/3/22.
+//  Copyright © 2019 zgy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ bsdiffDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  bsdiffDemo
+//
+//  Created by zgy on 2019/3/22.
+//  Copyright © 2019 zgy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
