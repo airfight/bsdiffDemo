20e3fc29e87d40a20cb6eaae3c4d0480272e1ed2
zgy朱光阳 <gy.zhu@ctrip.com>
2019-03-22 17:49:39 +0800

    init

--- bsdiffDemo/ViewController.m
+++ bsdiffDemo/ViewController.m
@@ -7,7 +7,7 @@
 //
 
 #import "ViewController.h"
-
+#import "bsdiffHeader.h"
 @interface ViewController ()
 
 @end
@@ -16,7 +16,87 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    
+//    [self bspatch];
+    
+    [self appBsdiff];
+    
+    [self appbspatch];
+        
+}
+
+- (void)appBsdiff {
+    
+    const char *argv[4];
+    
+    argv[0] = "bspatch";
+    
+    NSString *oldPath = [[NSBundle mainBundle] pathForResource:@"old-bundle.zip" ofType:nil];
+    
+    NSString *newPath = [[[NSBundle mainBundle] bundlePath] stringByAppendingPathComponent:@"new-bundle4.zip"];
+    
+    NSString *newPatch = [[NSBundle mainBundle] pathForResource:@"new.pat" ofType:nil];
+    
+    argv[1] = [oldPath UTF8String];
+    
+    argv[2] = [newPath UTF8String];
+    
+    argv[3] = [newPatch UTF8String];
+    
+    int result = bspatchmain(4, (char **)argv);
+    
+    NSLog(@"%d",result);
+    
+}
+
+- (void)appbspatch {
+    
+    const char *argv[4];
+    
+    argv[0] = "bspatch";
+    
+    NSString *oldPath = [[NSBundle mainBundle] pathForResource:@"old-bundle.zip" ofType:nil];
+    
+    NSString *newPath = [[[NSBundle mainBundle] bundlePath] stringByAppendingPathComponent:@"new-bundle4.zip"];
+    
+    NSString *newPatch = [[NSBundle mainBundle] pathForResource:@"new.pat" ofType:nil];
+
+    argv[1] = [oldPath UTF8String];
+
+    argv[2] = [newPath UTF8String];
+
+    argv[3] = [newPatch UTF8String];
+
+    int result = bspatchmain(4, (char **)argv);
+
+    NSLog(@"%d",result);
+    
+}
+
+- (void)bspatch {
+    
+    const char *argv[4];
+    
+    argv[0] = "bspatch";
+    
+    NSString *oldPath = @"/Users/zgy/Desktop/resources/bundle-rn.zip";
+    
+    NSString *newPatch = @"/Users/zgy/Desktop/resources/new.pat";
+    
+    NSString *newPath = @"/Users/zgy/Desktop/resources/new.zip";
+    
+    argv[1] = [oldPath UTF8String];
+    
+    argv[2] = [newPath UTF8String];
+    
+    argv[3] = [newPatch UTF8String];
+    
+    int result = bspatchmain(4, argv);
+    
+    NSLog(@"%d",result);
+    
 }
 
 
---
+++ bsdiffDemo/diffpatch/bsdiffHeader.h
@@ -0,0 +1,25 @@
+//
+//  bsdiffHeader.h
+//  bsdiffDemo
+//
+//  Created by zgy on 2019/3/22.
+//  Copyright © 2019 zgy. All rights reserved.
+//
+
+#ifndef bsdiffHeader_h
+#define bsdiffHeader_h
+
+
+//C++调用c方法 https://stackoverflow.com/questions/9334650/linker-error-calling-c-function-from-objective-c
+//#ifdef __cplusplus
+//extern "C" {
+//#endif
+// function
+//#ifdef __cplusplus
+//}
+//#endif
+
+int bsdiffmain(int argc,char *argv[]);
+int bspatchmain(int argc,char * argv[]);
+
+#endif /* bsdiffHeader_h */
